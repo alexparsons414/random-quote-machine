afbd168a1ac12ad1c1a3b589f1d9780bd5e0d355
alexparsons414 <alexparsons414@gmail.com>
2023-12-05 19:46:24 -0500

    fixed store, got messed up a little

--- src/features/store.js
+++ src/features/store.js
@@ -3,10 +3,6 @@ import quoteReducer from './quotes/quoteSlice';
 
 export default configureStore({
     reducer: {
-<<<<<<< HEAD
         quote: quoteReducer,
-=======
-        //TODO: IMPLEMENT STORE
->>>>>>> 35bc890755a602832bea59922c9c799d74e6d0d1
     },
 });
